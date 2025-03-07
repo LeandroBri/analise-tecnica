18bf3123d31c72ee6fd9c453a78bd34fd2208dc4
LeandroBri <34540660+LeandroBri@users.noreply.github.com>
2025-03-07 23:11:39 +0000

    Update fix_squeeze_pro.py

--- source/fix_squeeze_pro.py
+++ source/fix_squeeze_pro.py
@@ -3,7 +3,7 @@ with open('/opt/render/project/src/.venv/lib/python3.11/site-packages/pandas_ta/
     content = file.read()
 
 # Aqui você pode modificar o conteúdo conforme necessário (exemplo: tratando NaN)
-content = content.replace('NaN', '0')  # Modifique conforme necessário
+content = content.replace('NaN', 'nan')  # Modifique conforme necessário
 
 # Escreva o conteúdo de volta para o arquivo
 with open('/opt/render/project/src/.venv/lib/python3.11/site-packages/pandas_ta/momentum/squeeze_pro.py', 'w') as file:
