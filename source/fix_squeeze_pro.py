f7eee5206a02a9b24c7e727f2942c195b9b14da8
LeandroBri <34540660+LeandroBri@users.noreply.github.com>
2025-03-07 23:06:14 +0000

    Update fix_squeeze_pro.py
    Corrige o erro Nan apresentado ao tentar iniciar o Projeto

--- source/fix_squeeze_pro.py
+++ source/fix_squeeze_pro.py
@@ -1,1 +1,10 @@
+# fix_squeeze_pro.py
+with open('/opt/render/project/src/.venv/lib/python3.11/site-packages/pandas_ta/momentum/squeeze_pro.py', 'r') as file:
+    content = file.read()
 
+# Aqui você pode modificar o conteúdo conforme necessário (exemplo: tratando NaN)
+content = content.replace('NaN', '0')  # Modifique conforme necessário
+
+# Escreva o conteúdo de volta para o arquivo
+with open('/opt/render/project/src/.venv/lib/python3.11/site-packages/pandas_ta/momentum/squeeze_pro.py', 'w') as file:
+    file.write(content)
